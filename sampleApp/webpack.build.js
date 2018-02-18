d6b20e867753479b4d245b4c9564e21b3d25113f
jl818 <jl818@coolimage.com>
2018-02-18 14:48:51 -0500

    webpack build now can minify and is configurable

--- sampleApp/webpack.build.js
+++ sampleApp/webpack.build.js
@@ -1,4 +1,26 @@
 const path = require('path');
+const uglifyPlugin = require("webpack/lib/optimize/UglifyJsPlugin");
+
+// toggle the following 3 config settings to customize build
+const babel = true;
+const minify = true;
+const createMap = false;
+
+let loaders = [];
+if (babel) loaders.push({
+  exclude: /node_modules/,
+  loader: 'babel-loader',
+  query: {
+    presets: ['env'],
+  },
+});
+
+let plugins = [];
+if (minify) plugins.push(new uglifyPlugin({ minimize: true }));
+
+let devtools = undefined;
+if (createMap) devtools = 'source-map';
+
 
 module.exports = {
   entry: {
@@ -9,17 +31,9 @@ module.exports = {
     path: path.resolve(__dirname, 'build'),
     publicPath: "/",
   },
-  devtool: 'source-map',
-  plugins: [],
+  devtool: devtools,
+  plugins: plugins,
   module: {
-    loaders: [
-      {
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['env'],
-        },
-      },
-    ],
+    loaders: loaders,
   },
 };
